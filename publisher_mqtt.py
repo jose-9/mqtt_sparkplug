d0efd9c12a9e50055204fdc86201112711da38b3
zezé <jsampaioifce@gmail.com>
2024-11-20 00:51:32 -0300

    functional version of sparkplug been read inside Ignition

--- publisher_mqtt.py
+++ publisher_mqtt.py
@@ -2,6 +2,8 @@ import time
 import random
 import sys
 import os
+import socket
+
 from paho.mqtt import client as mqtt_client
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'tahu', 'python', 'core'))
@@ -15,48 +17,80 @@ except ImportError as e:
 
 
 # MQTT Broker details
-broker = 'broker.emqx.io'
+broker = "127.0.0.1"
+# broker = 'broker.emqx.io'
 port = 1883
-topic = "spBv1.0/mygroup_id/NBIRTH/my_node_id"
+# topic = "spBv1.0/mygroup_id/NBIRTH/my_node_id"
+topic = "spBv1.0/LARI/NBIRTH/nodetest"
 client_id = f'sparkplug_publisher_{random.randint(0, 1000)}'
 
 # Sparkplug B details
 namespace = "spBv1.0"  # Sparkplug namespace
-group_id = "mygroup_id"
-node_id = "my_node_id"
-device_id = "my_device_id"
+# group_id = "mygroup_id"
+# node_id = "my_node_id"
+# device_id = "my_device_id"
+
+# namespace = "sparkLARI"  # Sparkplug namespace
+group_id = "G20"
+node_id = "nodelet"
+device_id = "devv"
+
+username = 'admin'
+password = 'admin'
+
+# Connection test function
+def test_connection(ip, port):
+    try:
+        with socket.create_connection((ip, port), timeout=5):
+            print(f"Successfully connected to {ip}:{port}")
+    except Exception as e:
+        print(f"Could not connect to {ip}:{port}: {e}")
+
+# Verify connection
+test_connection(broker, port)
 
 
 def connect_mqtt():
     client = mqtt_client.Client(client_id=client_id)
+
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+   
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
     client.connect(broker, port)
     return client
 
 
-def create_payload():
+
+
+def create_payload(seq=0):
     """Creates a Sparkplug B payload for publishing"""
     payload = sparkplug_b_pb2.Payload()
 
     # Set timestamp
     payload.timestamp = int(time.time() * 1000)
 
+    # Set sequence number
+    payload.seq = seq
+
     # Add a metric (example)
     metric = payload.metrics.add()
-    metric.name = "temperature"
+    metric.name = ""
     metric.alias = 1
     metric.timestamp = payload.timestamp
-    # Assuming you want to use Int32 as the datatype for a metric
     metric.datatype = sparkplug_b_pb2.DataType.Int32
+    metric.int_value = random.randint(2, 30)  # Random temperature data
 
-    # metric.datatype = sparkplug_b_pb2.Payload.Metric.Int32
-    metric.int_value = random.randint(20, 30)  # Random temperature data
-
-    return payload
+    return payload,metric.int_value
 
 
 def publish(client):
     while True:
-        payload = create_payload()
+        payload,int_value = create_payload()
         # Serialize the payload to bytes for Sparkplug B
         payload_bytes = payload.SerializeToString()
         
@@ -64,7 +98,7 @@ def publish(client):
         result = client.publish(topic, payload_bytes)
         status = result[0]
         if status == 0:
-            print(f"Sent Sparkplug message to topic `{topic}`")
+            print(f"Sent Sparkplug {int_value} to topic `{topic}`")
         else:
             print(f"Failed to send message to topic {topic}")
         
--- subscriber_mqtt.py
+++ subscriber_mqtt.py
@@ -2,6 +2,8 @@ import time
 import random
 import sys
 import os
+import socket
+
 from paho.mqtt import client as mqtt_client
 # from mqtt_sparkplug import sparkplug_b_pb2
 # from sparkplug_b import sparkplug_b_pb2  # Import the generated Sparkplug B protobuf definitions
@@ -19,13 +21,41 @@ except ImportError as e:
 
 
 # MQTT Broker details
-broker = 'broker.emqx.io'
+broker = "127.0.0.1"
+# broker = 'broker.emqx.io'
 port = 1883
-topic = "spBv1.0/mygroup_id/NBIRTH/my_node_id"
+topic = "spBv1.0/#"
+# topic = "sparkLARI/G20/NBIRTH/nodelet"
+
+#NBIRTH/my_node_id"
 client_id = f'sparkplug_subscriber_{random.randint(0, 1000)}'
+
+username = 'admin'
+password = 'admin'
+
+# Connection test function
+def test_connection(ip, port):
+    try:
+        with socket.create_connection((ip, port), timeout=5):
+            print(f"Successfully connected to {ip}:{port}")
+    except Exception as e:
+        print(f"Could not connect to {ip}:{port}: {e}")
+
+# Verify connection
+test_connection(broker, port)
+
 
 def connect_mqtt():
     client = mqtt_client.Client(client_id=client_id)
+
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+   
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
     client.connect(broker, port)
     return client
 
